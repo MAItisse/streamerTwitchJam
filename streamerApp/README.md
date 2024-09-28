088a1899dad85c06262d997557eb9c4a964bd80d
Matisse <MatissesProjects@users.noreply.github.com>
2024-09-28 15:20:46 -0700

    initial commit of working placement app

---
+++ streamerApp/README.md
@@ -0,0 +1,13 @@
+# How to use
+- you can use the release section to grab an executable if you want
+- or you can run this directly through python main.py (suggested for full features)
+    - you will need to run `pip install -r requirements.txt` first
+- either way you will need to set a few settings in `settings.json`
+    - websocket obs password
+    - scene to effect
+    - name of sources to effect
+
+
+# interesting suggestions
+- You can use this to have multiple obs windows on different computers to output 1 screen onto another and modify all sources from 1 place
+    - even allowing chat to help
---
+++ streamerApp/main.py
@@ -0,0 +1,161 @@
+import obsws_python as obs
+import websocket
+import json
+import threading
+import requests
+import time
+
+password =                                                                                                                'Wqol0UimYRPwZttn'  # Use the password set in OBS WebSocket settings
+ws = obs.ReqClient(host='localhost', port=4455, password=password, timeout=3)
+IDs = []
+width = 1920
+height = 1080
+
+def getSceneItems(sceneName):
+    raw_request = {
+        "requestType": "GetSceneItemList",
+        "sceneName": sceneName,
+    }
+    response = ws.send('GetSceneItemList', data=raw_request, raw=True)
+    # print(response)
+    return response
+
+def getSelectedSceneItems(itemList, itemsToSelect):
+    selectedIds = []
+    for sceneItem in itemList['sceneItems']:
+        print(sceneItem['sceneItemId'], sceneItem['sourceName'])
+        if sceneItem['sourceName'] in itemsToSelect:
+            # print(sceneItem)
+            selectedIds.append(sceneItem['sceneItemId'])
+    return selectedIds
+
+def transformId(x: int, y: int, windowId: int):
+    raw_request = {
+        "requestType": "SetSceneItemTransform",
+        "sceneName": "Scene",
+        "sceneItemId": windowId,
+        "sceneItemTransform": {
+            "positionX": x,
+            "positionY": y,
+        }
+    }
+    print(f"transform Id request: {raw_request}")
+    response = ws.send('SetSceneItemTransform', data=raw_request, raw=True)
+
+def getWindowDetails(sceneName, sceneItemId):
+    raw_request = {
+        "requestType": "GetSceneItemTransform",
+        "sceneName": sceneName,
+        "sceneItemId": sceneItemId
+    }
+    response = ws.send('GetSceneItemTransform', data=raw_request, raw=True)
+    print(response)
+
+    sceneResponse = response['sceneItemTransform']
+    sizeOfWindow = ((float(sceneResponse['sourceWidth']) - float(sceneResponse['cropLeft'] + sceneResponse['cropRight'])) * sceneResponse['scaleX'],
+                    (float(sceneResponse['sourceHeight']) - float(sceneResponse['cropTop'] + sceneResponse['cropBottom'])) * sceneResponse['scaleY'])
+    locationOfWindow = (sceneResponse['positionX'], sceneResponse['positionY'])
+
+    print(f"sizeOfWindow: {sizeOfWindow}")
+    print(f"locationOfWindow: {locationOfWindow}")
+    return (sizeOfWindow, locationOfWindow)
+
+
+def getVideoOutputSettings():
+    raw_request = {
+        "requestType": "GetVideoSettings",
+    }
+    print(f"transform Id request: {raw_request}")
+    response = ws.send('GetVideoSettings', data=raw_request, raw=True)
+    return (response['baseWidth'], response['baseHeight'])
+
+
+def startWebsocketRoom(userId):
+    print("Starting WebSocket room with ID:", userId)
+    # Replace with your actual WebSocket server URL
+    postUrl = f'https://websocket.matissetec.dev/lobby/new?user={userId}'
+    print(postUrl)
+    response = requests.post(postUrl)
+    ws_url = f'wss://websocket.matissetec.dev/lobby/connect/streamer?user={userId}&key={response.text}'
+    print(ws_url)
+    ws_app = websocket.WebSocketApp(
+        ws_url,
+        on_open=on_open,
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close
+    )
+    ws_app.run_forever()
+
+
+def on_message(ws, message):
+    print("Received message:", message)
+    # Parse the message if it's in JSON format
+    if 'Hello Server!' in message:
+        # Receive new x and y positions
+        for windowId in IDs:
+            sizeOfWindow, locationOfWindow = getWindowDetails("Scene", windowId)
+            ws.send(json.dumps({"data":[
+                {
+                    "name": windowId,
+                    "x": locationOfWindow[0],
+                    "y": locationOfWindow[1],
+                    "width": f"{sizeOfWindow[0]}px",
+                    "height": f"{sizeOfWindow[1]}px",
+                    "info": "some data to register later"
+                }]}))
+        return
+    try:
+        data = json.loads(message)
+        x = data.get('x', .5)
+        y = data.get('y', .5)
+        windowId = int(data.get('name', 0))
+        
+        print("Getting new location for ID:", windowId)
+
+        # get id from name off list we create at beginning
+        transformId(x*float(width), y*float(height), windowId)
+        print("Moved window to", x, y)
+    except json.JSONDecodeError:
+        print("Received non-JSON message" + message)
+
+def on_error(ws, error):
+    print("WebSocket error:", error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("WebSocket connection closed")
+
+def on_open(ws):
+    print("WebSocket connection opened")
+    for windowId in IDs:
+        ws.send(json.dumps({"command": "get_positions", "id": windowId}))
+
+    def ping():
+        while True:
+            time.sleep(30)
+            ws.send("ping")
+    threading.Thread(target=ping, daemon=True).start()
+
+def getUserIdFromName(name):
+    url = f'https://decapi.me/twitch/id/{name}'
+    response = requests.get(url)
+    return response.text
+
+def main():
+    width, height = getVideoOutputSettings()
+    # print(f"player native width: {width}, height: {height}")
+
+    sceneItems = getSceneItems("Scene")
+    # print(sceneItems)
+
+    IDs = getSelectedSceneItems(sceneItems, ['gitEasy', 'gif'])
+    # print(IDs)
+
+    userId = getUserIdFromName("matissetec")
+    # print(userId)
+
+    listener_thread = threading.Thread(target=startWebsocketRoom, args=(userId,))
+    listener_thread.start()
+
+if __name__ == '__main__':
+    main()
